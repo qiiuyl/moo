796d883309f750bf11a97fa8e98764070ca443a2
qiuyl <279651740@qq.com>
2019-09-18 20:18:24 +0800

    play and singer

--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import Vue from "vue";
 import Router from "vue-router";
 import index from "./views/index"
+import singer from "./components/singer"
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {path: "/",component: index},
-    {path: "/index",component: index}
+    {path: "/index",component: index},
+    {path: "/singer",component:singer},
   ]
 });
